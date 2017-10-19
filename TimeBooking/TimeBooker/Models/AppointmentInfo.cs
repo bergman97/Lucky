e5eeb605f20346a56a5f7598652ba95c06a2ac18
Markus <Markus@MAKKANS-DATOR>
2017-10-19 19:54:02 +0200

    qwe

---
+++ TimeBooking/TimeBooker/Models/AppointmentInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DHTMLX.Scheduler;
+
+namespace TimeBooker.Models
+{
+    public class AppointmentInfo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DHXJson(Alias = "id")]
+        public int Id { get; set; }
+
+        [DHXJson(Alias = "text")]
+        public string NoteMessage { get; set; }
+
+        [DHXJson(Alias = "start_date")]
+        public DateTime StartDate { get; set; }
+
+        [DHXJson(Alias = "end_date")]
+        public DateTime EndDate { get; set; }
+    }
+}
